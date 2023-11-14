e1fa3eb43a4e01692bbf4f3c69f9de8007a973a4
karko13 <145532713+karko13@users.noreply.github.com>
2023-11-14 20:16:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrojanjeBrojeva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite neki broj koji zelite da se odbrojava");
+            int x = Convert.ToInt32(Console.ReadLine());
+            string lista = "";
+            for(int i = 1; i < x; i++) 
+            {
+                lista = lista + i ;
+                Console.WriteLine(lista );
+            }
+            Console.ReadKey();
+        }
+    }
+}
